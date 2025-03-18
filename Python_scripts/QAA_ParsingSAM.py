ec2b3b2bb5116a276e368bda944dead26c2253a5
kespinoz123 <kespinoz@uoregon.edu>
2025-03-17 22:07:18 -0700

    Parsing SAM file

--- Python_scripts/QAA_ParsingSAM.py
+++ Python_scripts/QAA_ParsingSAM.py
@@ -20,25 +20,26 @@ with open(args.file, 'r') as fh:
 		if line[0] != '@':
 			number_reads += 1
 	
-			#split first and then specify index 
+			# split first and then specify index 
 			line = line.split('\t')
 			flag = int(line[1])
-					#----checking for secondary alignment, so we have to verify 256 -----
-					#reasong being;
-					#You may encounter each read in a file more than once. 
-					#This will occur when you have multiple alignments for a single read. 
-					#You should be careful not to count reads as aligned more than once.
-			#not equal to 256
+					# ----checking for secondary alignment, so we have to verify 256 -----
+					# reasong being;
+					# You may encounter each read in a file more than once. 
+					# This will occur when you have multiple alignments for a single read. 
+					# You should be careful not to count reads as aligned more than once.
+			# not equal to 256
 			
 			if((flag & 256) != 256):
-				#Primary alignment 
-				#not equal to 4
+				# Primary alignment 
+				# not equal to 4
 				if((flag & 4) == 4):
 					number_unmapped += 1
 					
 				else:
 					number_mapped +=1
 					
+#  ----- Printing Statements  ----- 
 print("Number mapped: ", number_mapped)
 print("Number unmapped: ",number_unmapped)
 print("Number of reads: ",number_reads)
